70c2bf26576407de16a7809f1ec3367ec088a21f
Nick O <nostrowski79@gmail.com>
2015-07-15 21:43:47 -0400

    Resolving Warnings for unnecessary cast, removing settings menu logic, updating from depricated Activity type. Removed unnecessary menu layout.

--- app/src/main/java/com/example/nick/myportfolioapp/MainActivity.java
+++ app/src/main/java/com/example/nick/myportfolioapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.nick.myportfolioapp;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,7 +15,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +23,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         ArrayList<View> allButtons;
-        allButtons = ((RelativeLayout) findViewById(R.id.container)).getTouchables();
+        allButtons = (findViewById(R.id.container)).getTouchables();
         PorterDuffColorFilter orangeFilter = new PorterDuffColorFilter(Color.rgb(240,140,53), PorterDuff.Mode.SRC_ATOP);
         PorterDuffColorFilter redFilter = new PorterDuffColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
         for(View b : allButtons) {
@@ -32,28 +32,6 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             b.getBackground().setColorFilter(orangeFilter);
         }
         allButtons.get(allButtons.size() - 1).getBackground().setColorFilter(redFilter);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
